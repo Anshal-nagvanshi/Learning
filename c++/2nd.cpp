941aec17db3e7920403d82a8435c1320d157dcde
Anshal Nagvanshi <anshalnagvanshi1177@gmail.com>
2025-02-07 22:28:43 +0530

    Update 2nd.cpp

--- c++/2nd.cpp
+++ c++/2nd.cpp
@@ -1,9 +1,10 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 int main(){
-    string x;
-    cout<<"enter your name";
-    cin>>x;
-    cout<<"hello"<<x;
+    string str;
+    cout<<"may i know your name ";
+    getline(cin,str);
+    cout<<"Hello, "<<str;
+
     return 0;
 }
