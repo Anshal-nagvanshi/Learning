b0a571c21057e75913b22b22d22bd2bd68b31a44
Anshal Nagvanshi <anshalnagvanshi1177@gmail.com>
2025-02-08 14:35:08 +0530

    Create 4th.cpp

---
+++ c++/4th.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    float b, h, area;
+    cout<<"Enter the breadth and height";
+    cin>>b>>h;
+    area = (b*h)/2;
+    cout<<"area of triangle is:- "<<area;
+    return 0;
+}
